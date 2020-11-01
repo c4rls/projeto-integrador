e04c3047697e393c4f65265579b38abcdaf13c80
c4rls <ssantos.carloseduardo@gmail.com>
2020-11-01 09:56:04 -0300

    setup for kivy

--- projeto_integrador/__main__.py
+++ projeto_integrador/__main__.py
@@ -1,2 +1,13 @@
+from kivy.uix.label import Label
+from kivy.app import App
+import kivy
+kivy.require('1.11.1')
+
+
+class ProjetoIntegradorApp(App):
+    def build(self):
+        return Label(text='Projeto Integrador')
+
+
 if __name__ == '__main__':
-    print('Hello')
+    ProjetoIntegradorApp().run()
