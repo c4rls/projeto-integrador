06445340bd8bf107eb3340ce76ad3d866c8f2b98
c4rls <ssantos.carloseduardo@gmail.com>
2020-11-02 10:03:00 -0300

    simple screen to search for books

--- projeto_integrador/__main__.py
+++ projeto_integrador/__main__.py
@@ -1,12 +1,16 @@
-from kivy.uix.label import Label
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
 import kivy
 kivy.require('1.11.1')
 
 
+class ScreenRoot(BoxLayout):
+    pass
+
+
 class ProjetoIntegradorApp(App):
     def build(self):
-        return Label(text='Projeto Integrador')
+        return ScreenRoot()
 
 
 if __name__ == '__main__':
